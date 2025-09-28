a57ad3149be0cefc7a358ad9cd377a949bfad02b
Vokpolk <alexanderklopkov@gmail.com>
2025-09-28 19:13:58 +0300

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Александр Клопков on 28.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
